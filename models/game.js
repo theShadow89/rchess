9b5d3a8f4b691cd6dc508f1b788a32f6e41cad82
stefano <casto1189@gmail.com>
2016-04-19 00:00:54 +0200

    Add game model structure and unit test

---
+++ models/game.js
@@ -0,0 +1,25 @@
+var DB = require('../db');
+var uuid = require('node-uuid');
+
+var Chess = require('chess.js').Chess;
+
+
+exports.create = function(cb){
+    cb({});
+};
+
+exports.move = function(idGame,move,cb){
+    cb({});
+};
+
+exports.delete = function(idGame,cb){
+    cb({});
+};
+
+exports.list = function(cb){
+    cb([]);
+};
+
+exports.is_over = function(idGame,cb){
+    cb({});
+};
