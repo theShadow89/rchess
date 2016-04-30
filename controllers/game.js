c4bcfa4a6bcf6d54db44b9a505307e4374c40d21
stefano <casto1189@gmail.com>
2016-04-30 22:15:04 +0200

    Add game controller structure and unit test

---
+++ controllers/game.js
@@ -0,0 +1,16 @@
+var GameModel = require("../models/game");
+
+exports.create = function(req,res){
+};
+
+exports.move = function(req,res){
+};
+
+exports.status = function(req,res){
+};
+
+exports.delete = function(req,res){
+};
+
+exports.list = function(req,res){
+};
