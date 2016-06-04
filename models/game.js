f3597b48334466b31bad9a40a7c8c5eeb8b2eb85
shadow <casto1189@gmail.com>
2016-06-04 22:24:51 +0200

    change game model methods that check the games status stored on database

--- models/game.js
+++ models/game.js
@@ -114,37 +114,88 @@ exports.list = function (cb) {
 
 };
 
-exports.is_over = function(idGame,cb){
-    var game = DB.games[idGame];
-    cb({
-        game_over: game.game_over()
+exports.is_over = function (idGame, cb) {
+
+    //find game
+    this.findById(idGame, function (game, err) {
+        //if error occur return no data
+        if (err) {
+            cb({});
+        }
+
+        //create a game instance from fen stores on db
+        var chess = new Chess(game.fen);
+
+        cb({
+            game_over: chess.game_over()
+        });
     });
 };
 
-exports.is_draw = function(idGame,cb){
-    var game = DB.games[idGame];
-    cb({
-        draw: game.in_draw()
+exports.is_draw = function (idGame, cb) {
+    //find game
+    this.findById(idGame, function (game, err) {
+        //if error occur return no data
+        if (err) {
+            cb({});
+        }
+
+        //create a game instance from fen stores on db
+        var chess = new Chess(game.fen);
+
+        cb({
+            draw: chess.in_draw()
+        });
     });
 };
 
-exports.is_checkmate = function(idGame,cb){
-    var game = DB.games[idGame];
-    cb({
-        checkmate: game.in_checkmate()
+exports.is_checkmate = function (idGame, cb) {
+    //find game
+    this.findById(idGame, function (game, err) {
+        //if error occur return no data
+        if (err) {
+            cb({});
+        }
+
+        //create a game instance from fen stores on db
+        var chess = new Chess(game.fen);
+
+        cb({
+            checkmate: chess.in_checkmate()
+        });
     });
 };
 
-exports.is_check = function(idGame,cb){
-    var game = DB.games[idGame];
-    cb({
-        check: game.in_check()
+exports.is_check = function (idGame, cb) {
+    //find game
+    this.findById(idGame, function (game, err) {
+        //if error occur return no data
+        if (err) {
+            cb({});
+        }
+
+        //create a game instance from fen stores on db
+        var chess = new Chess(game.fen);
+
+        cb({
+            check: chess.in_check()
+        });
     });
 };
 
-exports.is_stalemate = function(idGame,cb){
-    var game = DB.games[idGame];
-    cb({
-        stalemate: game.in_stalemate()
+exports.is_stalemate = function (idGame, cb) {
+    //find game
+    this.findById(idGame, function (game, err) {
+        //if error occur return no data
+        if (err) {
+            cb({});
+        }
+
+        //create a game instance from fen stores on db
+        var chess = new Chess(game.fen);
+
+        cb({
+            stalemate: chess.in_stalemate()
+        });
     });
 };
