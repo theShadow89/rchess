7550afdf16b7695cbe5ac0a3ed565cce038ab10e
shadow <casto1189@gmail.com>
2016-06-23 00:19:55 +0200

    Add authenticator middleware to authenticate endpoint access

---
+++ middleware/authenticator.js
@@ -0,0 +1,33 @@
+module.exports=function(options){
+
+    var authenticate = function(req, res, next) {
+        // check header or url parameters or post parameters for token
+        var token = req.headers['x-access-token'];
+
+        // decode token
+        if (token) {
+
+            // verifies secret and checks exp
+            if(token != options.token) {
+                    return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                    // if everything is good, save to request for use in other routes
+                    req.token = token;
+                    next();
+            }
+
+        } else {
+            // if there is no token
+            // return an error
+            return res.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
+
+        }
+    };
+
+    return {
+        authenticate: authenticate
+    }
+};
