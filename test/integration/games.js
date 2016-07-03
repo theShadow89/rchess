3e2cb0a58087858506be4b8ad54dfdfd74ffd962
shadow <casto1189@gmail.com>
2016-07-03 21:04:55 +0200

    modify endpoints integration tests to include authentication

--- test/integration/games.js
+++ test/integration/games.js
@@ -1,6 +1,7 @@
 var expect = require('chai').expect,
     sinon = require('sinon');
 var request = require('supertest');
+process.env.TOKEN = 'testtoken';
 var app = require("../../app");
 
 var DB = require('../../db');
@@ -29,9 +30,10 @@ describe('Game Endpoint integration test', function() {
 
 
     describe('Game creation ', function() {
-        it('should create a game', function(done) {
+        it('should create a game with correct token', function(done) {
             server
                 .post('/api/games/create')
+                .set('x-access-token', 'testtoken')
                 // end handles the response
                 .end(function(err, res) {
                     if (err) {
@@ -45,12 +47,31 @@ describe('Game Endpoint integration test', function() {
                     done();
                 });
         });
+
+        it('should return error when token is wrong', function() {
+            server
+                .post('/api/games/create')
+                .set('x-access-token', 'wrongtoken')
+                // end handles the response
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.status).to.equal(200);
+
+                    expect(res.body.success).to.be.equal(false);
+                    expect(res.body.message).to.be.equal("Failed to authenticate token.");
+
+                });
+        });
     });
 
     describe('Game move ', function() {
-        it('should respond 200 status and valid flag to true if move in game is valid', function(done) {
+        it('should respond 200 status and valid flag to true if move in game is valid and correct token provided', function(done) {
             server
                 .put('/api/games/move/'+gameId)
+                .set('x-access-token', 'testtoken')
                 .send({move:{from:"c2",to:"c3"}})
                 // end handles the response
                 .end(function(err, res) {
@@ -64,12 +85,32 @@ describe('Game Endpoint integration test', function() {
                     done();
                 });
         });
+
+        it('should return error when token is wrong', function(done) {
+            server
+                .put('/api/games/move/'+gameId)
+                .set('x-access-token', 'wrongtoken')
+                .send({move:{from:"c2",to:"c3"}})
+                // end handles the response
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.status).to.equal(200);
+
+                    expect(res.body.success).to.be.equal(false);
+                    expect(res.body.message).to.be.equal("Failed to authenticate token.");
+                    done();
+                });
+        });
     });
 
     describe('List Games', function() {
-        it('should respond 200 status and array with all games id', function(done) {
+        it('should respond 200 status and array with all games id if a valid token provided', function(done) {
             server
                 .get('/api/games/')
+                .set('x-access-token', 'testtoken')
                 // end handles the response
                 .end(function(err, res) {
                     if (err) {
@@ -83,12 +124,31 @@ describe('Game Endpoint integration test', function() {
                     done();
                 });
         });
+
+        it('should return error when token is wrong', function(done) {
+            server
+                .get('/api/games/')
+                .set('x-access-token', 'wrongtoken')
+                // end handles the response
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.status).to.equal(200);
+
+                    expect(res.body.success).to.be.equal(false);
+                    expect(res.body.message).to.be.equal("Failed to authenticate token.");
+                    done();
+                });
+        });
     });
 
     describe('Status Game', function() {
-        it('should respond 200 status and game statuses array', function(done) {
+        it('should respond 200 status and game statuses array when token is correct', function(done) {
             server
                 .get('/api/games/status/'+gameId)
+                .set('x-access-token', 'testtoken')
                 // end handles the response
                 .end(function(err, res) {
                     if (err) {
@@ -102,12 +162,31 @@ describe('Game Endpoint integration test', function() {
                     done();
                 });
         });
+
+        it('should return error when token is wrong', function(done) {
+            server
+                .get('/api/games/status/'+gameId)
+                .set('x-access-token', 'wrongtoken')
+                // end handles the response
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.status).to.equal(200);
+
+                    expect(res.body.success).to.be.equal(false);
+                    expect(res.body.message).to.be.equal("Failed to authenticate token.");
+                    done();
+                });
+        });
     });
 
     describe('Delete Game', function() {
         it('should respond 200 status and games id if game will be deleted', function(done) {
             server
                 .delete('/api/games/delete/'+gameId)
+                .set('x-access-token', 'testtoken')
                 // end handles the response
                 .end(function(err, res) {
                     if (err) {
@@ -121,6 +200,25 @@ describe('Game Endpoint integration test', function() {
                     done();
                 });
         });
+
+        it('should return error when token is wrong', function(done) {
+            server
+                .delete('/api/games/delete/'+gameId)
+                .set('x-access-token', 'wrongtoken')
+                // end handles the response
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.status).to.equal(200);
+
+                    expect(res.body.success).to.be.equal(false);
+                    expect(res.body.message).to.be.equal("Failed to authenticate token.");
+                    done();
+                });
+        });
+
     });
 
 });
